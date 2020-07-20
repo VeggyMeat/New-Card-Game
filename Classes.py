cf3aa0f82ab4665d1ebc2a7c24f0036a45f941c6
Spamri123 <65345467+Spamri123@users.noreply.github.com>
2020-07-20 11:39:14 +0100

    Add files via upload

--- Classes.py
+++ Classes.py
@@ -1,12 +1,19 @@
 import pygame
 pygame.init()
+cards = []
 
 
 class card:
-    def __init__(self, x, y, screenX, screenY, used, image, imageSize, scaleWidth, scaleHeight):
+    def __init__(self, x, y, screenX, screenY, used, image, imageSize, scaleWidth, scaleHeight, name, description, ichorCost, exhaust=False):
         # integers between 0 and 4
         self.x = x
         self.y = y
+        # name of the card
+        self.name = name
+        # description of the card
+        self.description = description
+        # tells the cost of playing the card
+        self.ichorCost = ichorCost
         # co-ordinates of the screen
         self.screenX = screenX
         self.screenY = screenY
@@ -23,8 +30,10 @@ class card:
         self.resizedImage = image
         # resizes the image to the players monitor screen (hopefully)
         self.resize(scaleWidth, scaleHeight)
+        # a variable that tells if the card will be removed from the deck after being used
+        self.exhaust = exhaust
 
-    def draw(self, screen, scaleWidth, scaleHeight):
+    def draw(self, screen):
         screen.blit(self.resizedImage, (self.resizedX, self.resizedY))
 
     def resize(self, scaleWidth, scaleHeight):
@@ -32,3 +41,9 @@ class card:
         self.resizedY = int(self.screenY * scaleHeight)
         self.resizedImageSize = (int(self.imageSize[0] * scaleWidth), int(self.imageSize[1] * scaleHeight))
         self.resizedImage = pygame.transform.scale(self.image, self.resizedImage)
+
+    def use(self):
+        self.used()
+        if self.exhaust:
+            return True
+        return False
--- Main.py
+++ Main.py
@@ -2,17 +2,30 @@ from random import randint
 import pygame
 from pygame.locals import *
 from Classes import *
+from copy import deepcopy
 import time
+globalCounter = 0
 # sets up pygame
 pygame.init()
 # sets up pygame's clock system
 clock = pygame.time.Clock()
 # sets up the windowed screen and allowing things to resize when window size changed
-globalCounter = 0
 screen = pygame.display.set_mode((1920, 1080), RESIZABLE, VIDEORESIZE)
 WIDTH = 1920
 HEIGHT = 1080
+width, height = pygame.display.get_surface().get_size()
+scaleWidth = width / WIDTH
+scaleHeight = height / HEIGHT
 Open = True
+blankBoard = [
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0]
+             ]
+
+board = deepcopy(blankBoard)
 
 
 # some code to shuffle a deck of cards
@@ -22,23 +35,41 @@ def shuffle(deck):
         card = deck[randint(1, len(deck)-1)]
         newDeck.append(card)
         deck.remove(card)
+    return newDeck
+
+
+# code to allow two arrays to be entered with one being the board and the other where each card moves on a co-ordinate system and moves each one and allows them to loop
+def move(board, spot):
+    counter1 = 0
+    for row in board:
+        counter2 = 0
+        for Card in row:
+            if Card != 0:
+                thisSpot = spot[counter1][counter2]
+                board[counter1+thisSpot[1] % len(board)][counter2+thisSpot[0] % len(board)] = board[counter1][counter2]
+            counter2 += 1
+        counter1 += 1
+    return board
 
 
 # will draw all things on the screen
 def Draw():
-    pass
+    for row in board:
+        for Card in row:
+            if Card != 0:
+                Card.draw(screen)
+    pygame.display.update()
 
 
 # is called whenever the screen is resized
-def reSize():
-	width = event.w
-	height = event.h
-	scaleWidth = width / WIDTH
-	scaleHeight = height / HEIGHT
-	screen = pygame.display.set_mode((width, height), RESIZABLE, VIDEORESIZE)
+def reSize(event):
+    width = event.w
+    height = event.h
+    scaleWidth = width / WIDTH
+    scaleHeight = height / HEIGHT
+    screen = pygame.display.set_mode((width, height), RESIZABLE, VIDEORESIZE)
+    return width, height, scaleWidth, scaleHeight, screen
 
-
-reSize()
 while Open:
     # makes sure the game is running no faster than 60 fps
     clock.tick(60)
@@ -47,11 +78,11 @@ while Open:
     location = pygame.mouse.get_cursor()
     for event in pygame.event.get():
         # checks if the person has tried to close the window and closes the code
-        if event.type == event.QUIT:
+        if event.type == QUIT:
             Open = False
         # runs reSize when the window has been resized
-        elif event.type == event.VIDEORESIZE:
-            reSize()
+        elif event.type == VIDEORESIZE:
+            width, height, scaleWidth, scaleHeight, screen = reSize(event)
 
     Draw()
 pygame.quit()
